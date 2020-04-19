c6c988414d67a018ae38a56d7dd5df36cea7ffe3
David Sheinbein <davidsheinbein@Davids-MacBook-Pro.local>
2020-04-18 17:12:08 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,20 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"]
+const cardsInPlay = []
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+let cardOne = ["queen"]
+let cardTwo = ["king"]
+
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
+// from here below I get a bit lost. I'm not sure if its somthing fundamental or a misunderstanding of the instructions
+if (cardsInPlay.length === 2){
+console.log("User flipped queen")
+console.log("User flipped king")
+//mot sure how to fix this
+else if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
+	else (cardsInPlay !=== cardsInPlay)
+		alert("Sorry, try again.");
+}
