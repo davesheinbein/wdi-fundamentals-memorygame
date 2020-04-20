5a988999debe6c77e34a78f5a66cf26fa2a22bec
David Sheinbein <davidsheinbein@Davids-MacBook-Pro.local>
2020-04-19 17:03:10 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,50 +1,103 @@
 console.log("Up and running!");
 
-const cards = [
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-]
+var cards = [
+   { 
 
-const cardsInPlay = []
+  	rank: "queen",
+  	suit: "hearts",
+  	cardImage: "images/queen-of-hearts.png"
+
+  },
+   { 
+
+  	rank: "queen",
+  	suit: "diamonds",
+  	cardImage: "images/queen-of-diamonds.png"
+
+  },
+   { 
+
+  	rank: "king",
+  	suit: "hearts",
+  	cardImage: "images/king-of-hearts.png"
+
+  },
+  { 
+
+  	rank: "king",
+  	suit: "diamonds",
+  	cardImage: "images/king-of-diamonds.png"
+
+  }
+];
+
+var cardsInPlay = [];
+var plays = 0;
+var score = 0;
+
+//not sure if i should delete following 3 lines
 cardsInPlay.push(cards[cardId].rank);
-///is this correct
 console.log(cardImage)
 console.log(suit)
 
-function checkForMatch(){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  	console.log("You found a match!");
-	} 	
-	else {
-  	console.log("Sorry, try again.");
+// need help understanding functions, DOM, loops 
+function checkForMatch() { 
+	if(cardsInPlay.length === 2) {
+		// add something later to disable flipping after this condition is true
+		plays += 1; 
+		document.getElementById('plays-value').innerHTML = plays;
+        console.log("plays" + plays);
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+                score += 150; 
+                document.getElementById('score-value').innerHTML = score;
+                console.log("score" + score);
+                window.alert("You found a match!");
+                } else {
+                window.alert("Sorry, try again.");
+				}
+            }
+        }
+
+// need help understanding functions, DOM, loops
+
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
+
+
+// need help understanding functions, DOM, loops
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}	
 }
 
-// I think I missed a step im the control flow project 
-// This has made it difficult to move forward with the coding
+//Found help online via - https://github.com/mau-dev/wdi-fundamentals-memorygame/blob/master/memory-game/js/main.js
 
-
-
+var resetGame = function() {
+    while(cardsInPlay.length > 0) {
+    	cardsInPlay.pop();
+    	console.log(cardsInPlay);
+    }
+     resetCard = document.getElementsByTagName("img");    
+    for(i=0; i<resetCard.length; i++){
+      resetCard[i].setAttribute("src", "images/back.png");
+    }
+ // alert("new game");
 }
-function flipCard(cardId) {
-	console.log("User flipped" + cards[cardId].rank)
-}
 
-checkForMatch(flipCard);
+createBoard();
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', resetGame);
+console.log(cardsInPlay);
+
